527dc66fbc86b041b6687a5179cdfe22056fa5e4
Adriano Grimolizzi <adriano.grimolizzi@amadeus.com>
2019-11-27 20:07:58 +0100

    feature: move create-subscription to the bottom of the page, add animations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import { SocketIoConfig, SocketIoModule} from 'ngx-socket-io';
 
 import { AppComponent } from './app.component';
 import { SubscriptionListComponent } from './components/subscription-list/subscription-list.component';
 import { CreateSubscriptionComponent } from './components/create-subscription/create-subscription.component';
-import {SocketIoConfig, SocketIoModule} from 'ngx-socket-io';
 
 
 const config: SocketIoConfig = { url: 'http://localhost:4444', options: {} };
@@ -14,10 +16,11 @@ const config: SocketIoConfig = { url: 'http://localhost:4444', options: {} };
   declarations: [
     AppComponent,
     SubscriptionListComponent,
-    CreateSubscriptionComponent
+    CreateSubscriptionComponent,
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     SocketIoModule.forRoot(config)
   ],
--- src/app/components/create-subscription/create-subscription.component.ts
+++ src/app/components/create-subscription/create-subscription.component.ts
@@ -2,15 +2,26 @@ import { Component } from '@angular/core';
 
 import { Subscription } from '../../models/subscription';
 import {SubscriptionService} from '../../services/subscription.service';
+import {animate, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-create-subscription',
   templateUrl: './create-subscription.component.html',
-  styleUrls: ['./create-subscription.component.css']
+  styleUrls: ['./create-subscription.component.css'],
+  animations: [
+    trigger('slideIn', [
+      transition(':enter', [
+        style({transform: 'translateY(+100%)'}),
+        animate('380ms ease-in', style({transform: 'translateY(0%)'}))
+      ])
+    ])
+  ]
 })
 export class CreateSubscriptionComponent {
 
   public subscription: Subscription;
+
+  public isExpanded = false;
 
   constructor(private subscriptionService: SubscriptionService) {
     this.subscription = new Subscription();
@@ -25,5 +36,9 @@ export class CreateSubscriptionComponent {
     this.subscription.name = '';
     this.subscription.song = '';
     this.subscription.artist = '';
+  }
+
+  switchExpanded() {
+    this.isExpanded = !this.isExpanded;
   }
 }
